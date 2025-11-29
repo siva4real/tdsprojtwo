2810e46308f46e78cb2e2e62ac93f6b686f9042a
Abhinav Siva Ajith <abhiajith42059@gmail.com>
2025-11-29 13:42:59 +0530

    Delete LLMFiles directory

--- LLMFiles/runner.py
+++
@@ -1,7 +0,0 @@
-import speech_recognition as sr
-
-r = sr.Recognizer()
-with sr.AudioFile('demo-audio.opus') as source:
-    audio_data = r.record(source)
-    text = r.recognize_google(audio_data)
-    print(text)
